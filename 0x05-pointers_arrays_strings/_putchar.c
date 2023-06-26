5979aadc508186c993ed7e6769145bd266e0c177
kafastar <caarifwetu@gmail.com>
2023-06-26 11:10:34 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,10 +4,10 @@
  * _putchar - Writes a character to the standard output (stdout)
  * @c: The character to be written
  *
- * Return: On success, returns the character written as an unsigned char
- *         On error, returns -1
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1 and sets errno appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return write(1, &c, 1);
 }
