70f552b1b018d4926c061bd6676bfe149be7e8eb
kafastar <adikaahmed12@gmail.com>
2023-07-27 12:12:56 -0700

    Add node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,9 @@ size_t print_list(const list_t *h);
 /* Function prototype for list_len */
 size_t list_len(const list_t *h);
 
+/* Function prototype for add_node */
+list_t *add_node(list_t **head, const char *str);
+
 #endif /* LISTS_H */
 
 
