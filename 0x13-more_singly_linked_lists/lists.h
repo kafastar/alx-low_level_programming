73b300ca36fe7e79cc090d563cbc6a6cf2cb45a9
kafastar <adikaahmed12@gmail.com>
2023-08-01 11:12:21 -0700

    Sum list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,5 +34,8 @@ int pop_listint(listint_t **head);
 /* Function prototypes */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
+/* Function prototypes */
+int sum_listint(listint_t *head);
+
 #endif /* LISTS_H */
 
