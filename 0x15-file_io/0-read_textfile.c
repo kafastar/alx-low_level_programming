a465f99dc6b80e31dd803f526aed1ec8e56f140a
kafastar <adikaahmed12@gmail.com>
2023-08-08 11:40:12 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,36 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * readTextFile - Reads a text file and prints it to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
  *
- * Return: The actual number of letters read and printed.
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-ssize_t readTextFile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read_count, write_count;
-	char *buffer;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(fd);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
-	}
 
-	read_count = read(fd, buffer, letters);
-	if (read_count == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	write_count = write(STDOUT_FILENO, buffer, read_count);
-	free(buffer);
 	close(fd);
 
-	if (write_count < read_count)
-		return (0);
+	free(buf);
 
-	return (write_count);
+	return (nwr);
 }
